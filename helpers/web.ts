a3cfaced303a1cb1c9054377f587528f3e829f2f
Alexandr Burtovoy <volgobur@gmail.com>
2017-10-27 11:17:20 +0300

    fix env

--- helpers/web.ts
+++ helpers/web.ts
@@ -1,4 +1,5 @@
 import Web3 = require("web3")
+require('dotenv').config()
 
 const ETHEREUM_API = process.env.ETHEREUM_API
 if (!ETHEREUM_API) throw new Error('Please, set receiver address to ETHEREUM_API env variable')
